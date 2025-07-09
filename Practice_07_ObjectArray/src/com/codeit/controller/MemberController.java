598ea46e1060f4593c6cdc8c0365e5e5929ae49d
KarubiOhayo <pens9053@gmail.com>
2025-07-09 12:46:18 +0900

    feat : MemberController.java 클래스 구조 생성

--- Practice_07_ObjectArray/src/com/codeit/controller/MemberController.java
+++ Practice_07_ObjectArray/src/com/codeit/controller/MemberController.java
@@ -1,4 +1,57 @@
 package com.codeit.controller;
 
+import com.codeit.model.Member;
+
 public class MemberController {
+    Member[] mArray = new Member[MAX_SIZE];
+    public static int MAX_SIZE = 10;
+    int size; //추후 접근제어자 설정
+
+    public int existMemberNum() {
+
+    }
+
+    public Boolean checkId(String inputId) {
+
+    }
+
+    public void insertMember(String id, String name, String password, String email, String gender, int age) {
+
+    }
+
+    public Member searchId(String id) {
+
+    }
+
+    public Member[] searchName(String name) {
+
+    }
+
+    public Member[] searchEmail(String email) {
+
+    }
+
+    public Boolean updatePassword(String id, String password) {
+
+    }
+
+    public Boolean updateName(String id, String name) {
+
+    }
+
+    public Boolean updateEmail(String id, String email) {
+
+    }
+
+    public Boolean delete(String id) {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public Member[] printAll() {
+
+    }
 }
