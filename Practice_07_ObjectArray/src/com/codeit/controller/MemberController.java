37379d86d80662d0ccbdf5a61db96ebc77077f35
DESKTOP-GAFS2QL\K <pens9053@gmail.com>
2025-07-13 17:44:48 +0900

    Practice_07_ObjectArray 객체배열 과제 완료

--- Practice_07_ObjectArray/src/com/codeit/controller/MemberController.java
+++ Practice_07_ObjectArray/src/com/codeit/controller/MemberController.java
@@ -1,61 +1,144 @@
 package com.codeit.controller;
 
-import java.util.Arrays;
-
 import com.codeit.model.Member;
 
 public class MemberController {
-	public static int MAX_SIZE = 10;	// 배열의 최대 크기
-	private Member[] mArray = new Member[MAX_SIZE]; // 배열
+    public static final int MAX_SIZE = 10;    // 배열의 최대 크기
+    public static int size = 0;
+    private Member[] mArray = new Member[MAX_SIZE]; // 배열
 
-	public int existMemberNum() {
-	}
+    public MemberController() {
+        mArray[0] = new Member("test1", "1234", "홍길동", "aaa@aaa.aaa", 'M', 33);
+        mArray[1] = new Member("test2", "4321", "김길동", "bbb@bbb.bbb", 'M', 34);
+        mArray[2] = new Member("test3", "1234", "최길동", "ccc@ccc.ccc", 'M', 28);
+        mArray[3] = new Member("test4", "4321", "박길동", "ddd@ddd.ddd", 'M', 35);
+        mArray[4] = new Member("test5", "1234", "이길순", "eee@eee.eee", 'F', 17);
+        mArray[5] = new Member("test6", "1234", "박길순", "fff@fff.fff", 'F', 20);
+        mArray[6] = new Member("test7", "1234", "임길동", "ggg@ggg.ggg", 'M', 26);
+        mArray[7] = new Member("test8", "4321", "송길동", "hhh@hhh.hhh", 'M', 41);
+        mArray[8] = new Member("test9", "1234", "정길순", "iii@iii.iii", 'F', 28);
+        mArray[9] = new Member("test10", "4321", "양길동", "jjj@jjj.jjj", 'M', 23);
+        size = 10;
+    }
 
-	private int searchIdForIndex(String id) {
+    public int existMemberNum() {
+        return size;
+    }
 
-	}
+//	private int searchIdForIndex(String id) {
+//
+//	}
 
-	public boolean checkId(String inputId) {
+    public boolean checkId(String inputId) {
+        for (Member member : mArray) {
+            if (member.getId().equals(inputId)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	}
+    public Member searchId(String id) {
+        for (Member member : mArray) {
+            if (member.getId().equals(id)) {
+                return member;
+            }
+        }
+        return null;
+    }
 
-	public Member searchId(String id) {
+    public Member[] searchName(String name) {
+        Member[] m = new Member[size];
+        int mIndex = 0;
+        for (int i = 0; i < m.length; i++) {
+            if (mArray[i].getName().contains(name)) {
+                m[mIndex] = mArray[i];
+                mIndex++;
+            }
+        }
+        return m;
+    }
 
-	}
-
-	public Member[] searchName(String name) {
-
-	}
-
-	public Member[] searchEmail(String email) {
-
-	}
-
-
-	public boolean insertMember(String id, String name, String password, String email, char gender, int age){}
-
-	public boolean updatePassword(String id, String password) {
-	}
-
-	public boolean updateName(String id, String name) {
-
-	}
-
-	public boolean updateEmail(String id, String email) {
-
-	}
-
-	public boolean delete(String id) {
-
-	}
-
-	public void delete() {
-	}
-
-	public Member[] printAll() {
-	}
+    public Member[] searchEmail(String email) {
+        Member[] m = new Member[size];
+        int mIndex = 0;
+        for (int i = 0; i < m.length; i++) {
+            if (mArray[i].getEmail().contains(email)) {
+                m[mIndex] = mArray[i];
+                mIndex++;
+            }
+        }
+        return m;
+    }
 
 
+    public void insertMember(Member m) {
+        for (int i = 0; i < mArray.length; i++) {
+            if (mArray[i] == null) {
+                mArray[i] = m;
+                size++;
+                return;
+            }
+        }
+    }
+
+    public boolean updatePassword(String id, String password) {
+        for (int i = 0; i < size; i++) {
+            if (mArray[i].getId().equals(id)) {
+                mArray[i].setPassword(password);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean updateName(String id, String name) {
+        for (int i = 0; i < size; i++) {
+            if (mArray[i].getId().equals(id)) {
+                mArray[i].setName(name);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean updateEmail(String id, String email) {
+        for (int i = 0; i < size; i++) {
+            if (mArray[i].getId().equals(id)) {
+                mArray[i].setEmail(email);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean delete(String id) {
+        for (int i = 0; i < size; i++) {
+            if (mArray[i].getId().equals(id)) {
+                size--;
+                if (i < MAX_SIZE) {
+                    for (int j = i; j < MAX_SIZE - 1; j++) {
+                        mArray[j] = mArray[j + 1];
+                        if (j == size - 1) {
+                            mArray[j + 1] = null;
+                            break;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void delete() {
+        mArray = new Member[MAX_SIZE];
+        size = 0;
+    }
+
+    public Member[] printAll() {
+        return mArray;
+    }
 }
 
 
--- Practice_07_ObjectArray/src/com/codeit/model/Member.java
+++ Practice_07_ObjectArray/src/com/codeit/model/Member.java
@@ -1,5 +1,83 @@
 package com.codeit.model;
 
 public class Member {
+    private String id;
+    private String password;
+    private String name;
+    private String email;
+    private char gender;
+    private int age;
 
+    public Member() {
+    }
+
+    public Member(String id, String password, String name, String email, char gender, int age) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Member{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append(", gender=").append(gender);
+        sb.append(", age=").append(age);
+        sb.append('}');
+        return sb.toString();
+    }
 }
